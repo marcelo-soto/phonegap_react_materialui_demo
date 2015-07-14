9bdd63a7e000dc35b667729835b3f61e9c297af2
Marcelo Soto Montes <cabyrus@gmail.com>
2015-07-13 23:58:39 -0400

    some stuff

--- www/js/components/main.js
+++ www/js/components/main.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var mui = require('material-ui');
 var RaisedButton = mui.RaisedButton;
+var DropDownMenu = mui.DropDownMenu;
+var AppBar = mui.AppBar;
 var ThemeManager = new mui.Styles.ThemeManager();
 var Colors = mui.Styles.Colors;
 
@@ -29,12 +31,26 @@ var Main = React.createClass({displayName: "Main",
       paddingTop: '200px'
     };
 
+
+    var menuItems = [
+   { payload: '1', text: 'Never' },
+   { payload: '2', text: 'Every Night' },
+   { payload: '3', text: 'Weeknights' },
+   { payload: '4', text: 'Weekends' },
+   { payload: '5', text: 'Weekly' },
+];
+
+
+
     return (
       React.createElement("div", {style: containerStyle}, 
 
         React.createElement("h1", null, "material-ui"), 
         React.createElement("h2", null, "example project"), 
-
+React.createElement(AppBar, {
+  title: "Title", 
+  iconClassNameRight: "muidocs-icon-navigation-expand-more"}), 
+        React.createElement(DropDownMenu, {menuItems: menuItems}), 
         React.createElement(RaisedButton, {label: "Super Secret Password", primary: true, onTouchTap: this._handleTouchTap})
 
       )
--- www/jsx/components/main.js
+++ www/jsx/components/main.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var mui = require('material-ui');
 var RaisedButton = mui.RaisedButton;
+var DropDownMenu = mui.DropDownMenu;
+var AppBar = mui.AppBar;
 var ThemeManager = new mui.Styles.ThemeManager();
 var Colors = mui.Styles.Colors;
 
@@ -29,12 +31,26 @@ var Main = React.createClass({
       paddingTop: '200px'
     };
 
+
+    var menuItems = [
+   { payload: '1', text: 'Never' },
+   { payload: '2', text: 'Every Night' },
+   { payload: '3', text: 'Weeknights' },
+   { payload: '4', text: 'Weekends' },
+   { payload: '5', text: 'Weekly' },
+];
+
+
+
     return (
       <div style={containerStyle}>
 
         <h1>material-ui</h1>
         <h2>example project</h2>
-
+<AppBar
+  title="Title"
+  iconClassNameRight="muidocs-icon-navigation-expand-more" />
+        <DropDownMenu menuItems={menuItems} />
         <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
 
       </div>
